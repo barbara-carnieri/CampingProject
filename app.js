4dcf7ebbbc7a87898faf8d4ff975c8099563d5d7
barbara-carnieri <barbaracarnieri@yahoo.com>
2019-10-20 17:59:09 +0200

    first commit

--- app.js
+++ app.js
@@ -50,11 +50,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-
-
-
-
-
 app.listen(3000, function(){
     console.log("The YelpCamp Server Has Started!");
 });
